8d59420adc9163c7b64da7edc3fb03b2b1fa4fba
cnstrct0x <cnstrct0x@gmail.com>
2021-04-25 15:19:26 +0200

    :alembic: LAMBRO token

---
+++ contracts/Lambro.sol
@@ -0,0 +1,23 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.3;
+
+contract Lambro {
+  string public name = "Lambro";
+  string public symbol = "LAMBRO";
+  uint public totalSupply = 1000000;
+  mapping (address => uint) balances;
+
+  constructor() {
+    balances[msg.sender] = totalSupply;
+  }
+
+  function transfer(address to, uint amount) external {
+    require(balances[msg.sender] >= amount, "Not enough tokens");
+    balances[msg.sender] -= amount;
+    balances[to] += amount;
+  }
+
+  function balanceOf(address account) external view returns (uint) {
+    return balances[account];
+  }
+}
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,6 +1,10 @@
 const hre = require("hardhat");
 
 async function main() {
+  const Lambro = await hre.ethers.getContractFactory("Lambro");
+  const lambro = await Lambro.deploy();
+  await lambro.deployed();
+  console.log("Lambro deployed to:", lambro.address);
 }
 
 main()
