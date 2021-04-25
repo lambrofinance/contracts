e51f66ced5cd244ebb3765cb73ea87ffe1013db3
cnstrct0x <cnstrct0x@gmail.com>
2021-04-25 16:06:26 +0200

    :sparkles: Use ERC20 token standard

--- contracts/Lambro.sol
+++ contracts/Lambro.sol
@@ -1,23 +1,10 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.3;
 
-contract Lambro {
-  string public name = "Lambro";
-  string public symbol = "LAMBRO";
-  uint public totalSupply = 1000000;
-  mapping (address => uint) balances;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-  constructor() {
-    balances[msg.sender] = totalSupply;
-  }
-
-  function transfer(address to, uint amount) external {
-    require(balances[msg.sender] >= amount, "Not enough tokens");
-    balances[msg.sender] -= amount;
-    balances[to] += amount;
-  }
-
-  function balanceOf(address account) external view returns (uint) {
-    return balances[account];
+contract Lambro is ERC20 {
+  constructor() ERC20("Lambro", "LAMBRO") {
+    _mint(msg.sender, 1000000 * (10 ** 18));
   }
 }
